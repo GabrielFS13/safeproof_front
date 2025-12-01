24517f4c9724faa3390e169bf6413a6e3338185f
Gabriel_FS13 <falcaoshadowbr@gmail.com>
2025-12-01 20:51:56 -0300

    filtros e bug fix

--- src/pages/OcorrenciasPage.jsx
+++ src/pages/OcorrenciasPage.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import Ocorrencia from '../components/Ocorrencia';
+import { RxReset } from "react-icons/rx";
 
 const OcorrenciasPage = ({ userType }) => {
 
     const [ocorrencias, setOcorrencias] = useState([]);
+    const [ocorrenciasOriginal, setOcorrenciasOriginal] = useState([])
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const [listaCrimes, setTipo_crimes] = useState([])
+    const [msg, setMsg] = useState('Você ainda não criou nenhuma ocorrência.')
     const API_URL = process.env.REACT_APP_API_URL;
 
     const endpoint = userType === "PROFISSIONAL" ? 'get_ocorrencia_com_evicendencias_ativa_responsavel' : 'get_ocorrencias_com_evicendencias'
@@ -24,6 +27,7 @@ const OcorrenciasPage = ({ userType }) => {
 
                 const ocorrencias = await getOcorrencias.json()
                 setOcorrencias(ocorrencias.ocorrencias)
+                setOcorrenciasOriginal(ocorrencias.ocorrencias)
 
             } catch (err) {
                 setError(err.message);
@@ -55,19 +59,109 @@ const OcorrenciasPage = ({ userType }) => {
 
 
     if (isLoading) {
-        return <div style={{ padding: '2rem', color: 'black' }}><h2>Carregando suas ocorrências...</h2></div>;
+        return <div style={{ padding: '2rem', color: 'black', margin: '120px' }}><h2>Carregando suas ocorrências...</h2></div>;
     }
 
     if (error) {
-        return <div style={{ padding: '2rem', color: 'red' }}><h2>Erro: {error}</h2></div>;
+        return <div style={{ padding: '2rem', color: 'red', margin: '120px' }}><h2>Erro: {error}</h2></div>;
+    }
+
+
+    const handleDateFilter = (e) => {
+        if (e.target.value === "recentes") {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    new Date(b.ocorrencia.created_at) - new Date(a.ocorrencia.created_at)
+                )]
+            );
+
+        } else {
+            setOcorrencias(
+                [...ocorrencias.sort((a, b) =>
+                    new Date(a.ocorrencia.created_at) - new Date(b.ocorrencia.created_at)
+                )]
+            );
+
+        }
+    }
+
+    const handleAtivas = (e) => {
+
+        const ordem = e.target.value.toUpperCase()
+        setMsg('Não existe nenhuma ocorrência com esse filtro')
+        console.log(ocorrencias)
+        setOcorrencias(ocorrenciasOriginal.filter(({ ocorrencia }) =>
+            ocorrencia.status == ordem))
+
+
+    }
+
+    const handleWithProfessionals = (e) => {
+
+        const filtro = e.target.value
+
+        setMsg('Não existe nenhuma ocorrência com profissional designado.')
+
+        if (filtro == 'com') {
+            setOcorrencias(ocorrenciasOriginal.filter(({ ocorrencia }) =>
+                ocorrencia.id_responsavel !== ocorrencia.id_usuario))
+        } else {
+            setOcorrencias(ocorrenciasOriginal.filter(({ ocorrencia }) =>
+                ocorrencia.id_responsavel === ocorrencia.id_usuario))
+        }
+    }
+
+    const handleReset = () =>{
+        setOcorrencias([...ocorrenciasOriginal])
     }
 
 
     return (
         <div style={{ padding: '2rem', color: 'black', marginTop: '30px' }}>
             <h2>Minhas Ocorrências</h2>
+            <menu className='ocorrencias_filtros'>
+                <ul>
+                    <li>
+                        <select onChange={handleDateFilter}>
+                            <option value="recentes">
+                                Mais Recentes
+                            </option>
+                            <option value="antigas">
+                                Mais antigas
+                            </option>
+                        </select>
+                    </li>
+                    <li>
+                        <select onChange={handleAtivas}>
+                            <option value="ativa">
+                                Ativas
+                            </option>
+                            <option value="arquivadas">
+                                Arquivadas
+                            </option>
+                        </select>
+                    </li>
+                    <li>
+                        <select onChange={handleWithProfessionals}>
+                            <option value="com">
+                                Com profissional
+                            </option>
+                            <option value="sem">
+                                Sem profissional
+                            </option>
+                        </select>
+                    </li>
+                    <li>
+                        <RxReset
+                        size={30} 
+                        style={{cursor: "pointer"}}
+                        onClick={handleReset}
+                        />
+                    </li>
+                </ul>
+            </menu>
             {ocorrencias.length === 0 ? (
-                <p>Você ainda não criou nenhuma ocorrência.</p>
+                <p>{msg}</p>
             ) : (
                 <section className='minhas_ocorrencias'>
                     {listaCrimes ? ocorrencias.map((ocorrencia) => (
